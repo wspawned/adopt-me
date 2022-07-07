3197cff292da1f734525928322b0cc9b8c2cde20
wspawned <wspawned@gmail.com>
2022-07-08 02:57:26 +0300

    done n ready to deploy

--- src/Details.js
+++ src/Details.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import Modal from "./Modals";
 
 class Details extends Component {
     // constructor(props) {
@@ -11,7 +12,7 @@ class Details extends Component {
     //     this.state = { loading: true };
     // }                                      //////// below line is same thanks to Babel but why ???
     
-    state = { loading: true }
+    state = { loading: true, showModal: false }
 
     async componentDidMount() {
         const res = await fetch(
@@ -23,12 +24,14 @@ class Details extends Component {
         //// this.setState(Object.assign({ loading: false }, json.pets[0]));    same as above
     }
 
+    toggleModal = () => this.setState( { showModal: !this.state.showModal })
+
     render() {
         if (this.state.loading) {
             return <h2>loading...</h2>;
         }
 
-        const { animal, breed, city, state, description, name, images } = this.state;
+        const { animal, breed, city, state, description, name, images, showModal } = this.state;
 
         return (
             <div className="details">
@@ -41,13 +44,26 @@ class Details extends Component {
                     <ThemeContext.Consumer>
                         {
                             ([theme]) => (
-                                <button style={{backgroundColor: theme}}>
+                                <button 
+                                onClick={this.toggleModal}
+                                style={{backgroundColor: theme}}>
                                     Adopt {name}
                                 </button>
                             )
                         }
                     </ThemeContext.Consumer>
                     <p>{description}</p>
+                    {showModal ? (
+                        <Modal>
+                            <div>
+                                <h1>Would you like to adopt {name}?</h1>
+                                <div className="buttons">
+                                    <a href="https://bit.ly/pet-adopt">Yes</a>
+                                    <button onClick={this.toggleModal}>No</button>
+                                </div>   
+                            </div>
+                        </Modal>
+                       ) : null}
                 </div>
             </div>
         );
